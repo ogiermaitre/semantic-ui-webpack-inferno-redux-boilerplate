324dbe9a287be8d83aecb35416565a994c877e43
Unknown <24251307+ogiermaitre@users.noreply.github.com>
2018-03-22 16:36:53 +0100

    Some code styling (eslint)

--- src/components/VersionComponent.jsx
+++ src/components/VersionComponent.jsx
@@ -1,15 +1,13 @@
-import {version} from 'inferno';
+import { version } from 'inferno';
 
-function showVersion() {
-	alert(`The version is: ${ version }!`);
+const showVersion = () => {
+	alert(`The version is: ${version}!`);
 }
 
-export default function VersionComponent() {
-	return (
-		<div class="ui container segment padded header">
-			<p>This is an Inferno Boilerplate example using <em>Inferno { version }</em>.</p>
-			<button onClick={ showVersion }>Show version</button>
-			<div style='height:500px'></div>
-		</div>
-	);
-}
+export default () =>
+	<div class='ui container segment padded header'>
+		<p>This is an Inferno Boilerplate example using <em>Inferno {version}</em>.</p>
+		<button onClick={showVersion}>Show version</button>
+		<div style='height:500px'></div>
+	</div>
+
